4e2e4edbd9c34b0bc114dfe17e62d5248fc33b42
Partha Ghosh <partha31415@gmail.com>
2022-01-05 18:51:30 +0100

    some changes made 4

--- 3d/model.py
+++ 3d/model.py
@@ -65,7 +65,7 @@ class UNET(nn.Module):
             concat_skip = torch.cat((skip_connection, x), dim=1)
             x = self.ups[idx+1](concat_skip)
 
-        return self.final_conv(x)
+        return self.final_conv(x) 
 
 def test():
     x = torch.randn((4, 1, 96, 96, 96))
